4d289d831c086d29e4b7ea93eba5710e07b2250d
Matheus Bernareds <12648924+mthbernardes@users.noreply.github.com>
2020-06-28 03:07:16 -0300

    add plugins

--- project.clj
+++ project.clj
@@ -1,6 +1,19 @@
 (defproject shaggy-rogers "0.1.0-SNAPSHOT"
   :description "Clojure lambda which knows how to scan blob files and find sensitive information."
   :url "https://github.com/mthbernardes/shaggy-rogers"
+
+  :repositories [["central" {:url "https://repo1.maven.org/maven2/" :snapshots false}]
+                 ["clojars" {:url "https://clojars.org/repo/"}]]
+
+  :plugins [[lein-cloverage "1.0.13" :exclusions [org.clojure/clojure]]
+            [lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
+            [lein-kibit "0.1.7" :exclusions [org.clojure/clojure]]
+            [lein-cljfmt "0.6.1" :exclusions [org.clojure/clojure rewrite-clj org.clojure/tools.reader org.clojure/tools.cli]]
+            [jonase/eastwood "0.3.5" :exclusions [org.clojure/clojure]]
+            [s3-wagon-private "1.3.2" :exclusions [commons-logging org.apache.httpcomponents/httpclient]]
+            [lein-ancient "0.6.15" :exclusions [com.fasterxml.jackson.core/jackson-databind com.fasterxml.jackson.core/jackson-core]]
+            [lein-nsorg "0.2.0" :exclusions [rewrite-clj]]]
+
   :dependencies [[org.clojure/clojure                "1.10.1"]
                  [org.clojure/data.json              "0.2.6"]
                  [clj-jwt                            "0.1.1"]
@@ -12,5 +25,6 @@
                  [com.cognitect.aws/endpoints        "1.1.11.789"]
                  [com.cognitect.aws/s3               "799.2.682.0"]]
   :aot :all
+
   :aliases {"lint"                                       ["do" ["cljfmt" "check"] ["nsorg"] ["eastwood" "{:namespaces [:source-paths]}"]]
             "lint-fix"                                   ["do" ["cljfmt" "fix"] ["nsorg" "--replace"]]})
