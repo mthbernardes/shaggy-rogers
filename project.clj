8e0944804ceed1a384b279a70406280b5142efd5
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2020-07-08 14:52:18 -0300

    add pedantic (#9)

--- project.clj
+++ project.clj
@@ -14,16 +14,18 @@
             [lein-ancient "0.6.15" :exclusions [com.fasterxml.jackson.core/jackson-databind com.fasterxml.jackson.core/jackson-core]]
             [lein-nsorg "0.2.0" :exclusions [rewrite-clj]]]
 
-  :dependencies [[org.clojure/clojure                "1.10.1"]
-                 [org.clojure/data.json              "0.2.6"]
-                 [clj-jwt                            "0.1.1"]
-                 [mockfn                             "0.5.0"]
-                 [clj-http                           "3.10.1"]
-                 [com.amazonaws/aws-lambda-java-core "1.2.0"]
-                 [com.novemberain/pantomime          "2.11.0"]
-                 [com.cognitect.aws/api              "0.8.456"]
-                 [com.cognitect.aws/endpoints        "1.1.11.789"]
-                 [com.cognitect.aws/s3               "799.2.682.0"]]
+  :pedantic? :abort
+
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "1.0.0"]
+                 [clj-jwt "0.1.1"]
+                 [mockfn "0.5.0"]
+                 [clj-http "3.10.1"]
+                 [com.amazonaws/aws-lambda-java-core "1.2.1"]
+                 [com.novemberain/pantomime "2.11.0" :exclusions [org.bouncycastle/bcpkix-jdk15on org.bouncycastle/bcprov-jdk15on]]
+                 [com.cognitect.aws/api "0.8.456" :exclusions [org.clojure/data.json]]
+                 [com.cognitect.aws/endpoints "1.1.11.813"]
+                 [com.cognitect.aws/s3 "799.2.682.0"]]
   :aot :all
 
   :aliases {"lint"                                       ["do" ["cljfmt" "check"] ["nsorg"] ["eastwood" "{:namespaces [:source-paths]}"]]
